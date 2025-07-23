1641d88b89344b864472e57887a3b72aa27abdd5
Ahmed Etman <MrEtmanAhmed@gmail.com>
2025-07-23 16:36:16 +0300

    check the score value > 0

--- actions/storeScore.php
+++ actions/storeScore.php
@@ -9,15 +9,18 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && (isset($_POST['add'])))
     $score = (int) $_POST['add'];
     $type = $_POST['type'];
 
-    print_r($score . $type);
-
+if ($scire>0)
+{
     $sql = "INSERT INTO `team` (`score`,`type`) VALUES ($score ,'$type');";
     $result = mysqli_query($conn,$sql);
 
     if (!$result) {
         die("خطأ في التنفيذ: " . mysqli_error($conn));
     }
+}
 header("location:../index.php");
 exit;
+
+
 }
 ?>
